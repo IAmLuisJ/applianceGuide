493c86fbb46e916485317a3288f176fb8c070496
Johnny Lee <leejohnny47@gmail.com>
2023-05-16 18:16:22 -0400

    feat: styling of site content v1

--- app/components/Appliancequestionform.jsx
+++ app/components/Appliancequestionform.jsx
@@ -12,7 +12,7 @@ import { Link } from "@remix-run/react";
 
 export default function Appliancequestionform() {
   return (
-    <div class="mt-8 grid grid-cols-1 items-center gap-4 rounded-lg bg-blue-700 py-12 px-14 md:grid-cols-2">
+    <div class="my-8 grid grid-cols-1 items-center gap-4 rounded-lg bg-bby-blue py-12 px-14 md:grid-cols-2">
       <div class="text-center md:text-left">
         <div class="text-4xl font-extrabold text-white">
           What are you
@@ -22,7 +22,7 @@ export default function Appliancequestionform() {
         <div class="mt-2 text-sm text-violet-100">
           Answer a few quick questions so we can provide our best recommendation
         </div>
-        <ul class="mt-6 mb-8 text-sm text-violet-100 md:mb-0">
+        <ul class="mt-4 mb-8 text-sm text-violet-100 md:mb-0">
           <li class="flex items-center justify-center md:justify-start">
             <svg
               preserveAspectRatio="xMidYMid meet"
@@ -65,7 +65,7 @@ export default function Appliancequestionform() {
       </div>
       <form
         id="questions"
-        class="flex flex-col md:ml-8"
+        class="flex flex-col md:ml-8 text-white"
         action="https://www.bestbuy.com/site/searchpage.jsp"
         method="GET"
       >
@@ -76,34 +76,68 @@ export default function Appliancequestionform() {
           value="installdays_facet=Delivery + Install~Within 2 days"
         />
         <input type="hidden" name="st" value="range" />
-        <input
-          class="mb-4 rounded-md px-4 py-3 font-light"
-          min="0"
-          placeholder="What is your zip code?"
-          type="number"
-        />
-        <input type="checkbox" name="install" />
-        <label for="install">I want installation.</label>
+
         <fieldset>
-          <legend>Fuel Type</legend>
-          <label for="ftype">Electric</label>
-          <input type="radio" id="ftypeh" name="fuel_type" value="Electric" />
-          <br />
-          <label for="css">Gas</label>
-          <input type="radio" id="ftypec" name="fuel_type" value="Gas" />
-          <br />
-          <label for="LP">Liquid Propane</label>
-          <input type="radio" id="javasftypej" name="fuel_type" value="LP" />
+          <legend className="text-sm font-semibold leading-6">Fuel Type</legend>
+          <div className="ml-4">
+            <div className="flex items-center gap-x-3">
+              <input id="fuel_type_electric" name="fuel_type" type="radio" value="fuel_type_electric"
+                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"></input>
+              <label htmlFor="fuel_type_electric"
+                     className="block text-sm font-medium leading-6">Electric</label>
+            </div>
+            <div className="flex items-center gap-x-3">
+              <input id="fuel_type_gas" name="fuel_type" type="radio" value="fuel_type_gas"
+                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"></input>
+              <label htmlFor="fuel_type_gas"
+                     className="block text-sm font-medium leading-6">Gas</label>
+            </div>
+            <div className="flex items-center gap-x-3">
+              <input id="fuel_type_lp" name="fuel_type" type="radio" value="fuel_type_lp"
+                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"></input>
+              <label htmlFor="fuel_type_lp"
+                     className="block text-sm font-medium leading-6">Liquid Propane</label>
+            </div>
+          </div>
         </fieldset>
-        <input type="checkbox" name="Color" />
-        <label for="install">I have color preferences.</label>
-        <input type="checkbox" name="Brand" />
-        <label for="install">I have brand preferences.</label>
-        <input type="checkbox" name="haulaway" />
-        <label for="install">I want my old appliances hauled away.</label>
+
+        <div className="mt-3 space-y-6">
+          <div className="flex items-center gap-x-3">
+            <input type="checkbox" name="install" />
+            <label htmlFor="install" >I want installation.</label>
+          </div>
+        </div>
+
+        <div className="mt-1 space-y-6">
+          <div className="flex items-center gap-x-3">
+            <input type="checkbox" name="haulaway" />
+            <label htmlFor="haulaway" >I want my old appliances hauled away.</label>
+          </div>
+        </div>
+
+        <div className="mt-1 space-y-6">
+          <div className="flex items-center gap-x-3">
+            <input type="checkbox" name="Color" />
+            <label htmlFor="Color" >I have color preferences.</label>
+          </div>
+        </div>
+
+        <div className="mt-1 space-y-6">
+          <div className="flex items-center gap-x-3">
+            <input type="checkbox" name="Brand" />
+            <label htmlFor="Brand" >I have brand preferences.</label>
+          </div>
+        </div>
+
+        <input
+            className="mt-6 mb-4 rounded-md px-4 py-3 font-light"
+            min="0"
+            placeholder="What is your zip code?"
+            type="number"
+        />
         <button
-          class="flex items-center justify-center rounded-md bg-blue-900 px-4 py-3 font-semibold text-white shadow-md shadow-blue-700 transition hover:bg-slate-900"
-          type="submit"
+            className="flex items-center justify-center rounded-md bg-bby-yellow px-4 py-3 text-black font-semibold shadow-md shadow-violet-700 transition hover:bg-bby-yellow-dk"
+            type="submit"
         >
           <svg
             preserveAspectRatio="xMidYMid meet"
@@ -123,6 +157,7 @@ export default function Appliancequestionform() {
           </svg>
           Find the best options
         </button>
+
       </form>
     </div>
   );
